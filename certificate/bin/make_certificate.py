058d32fff0e514446ff730ccbf506f8cf5c7236a
Mathew Biddle <8480023+MathewBiddle@users.noreply.github.com>
2023-05-05 10:47:27 -0400

    cleaning up imports and running black

--- certificate/bin/make_certificate.py
+++ certificate/bin/make_certificate.py
@@ -1,38 +1,45 @@
-from jinja2 import Environment, FileSystemLoader
-# import argparse
-import json
 import os
-import shutil
-import sys
+from jinja2 import Environment, FileSystemLoader
+
 
 def write_svg_cert(template, config):
     root = os.path.dirname(os.path.abspath(__file__))
-    #root = path to output directory
-    fname = config['name']['first_name']+'_'+config['name']['last_name']+'_'+config['date']+'.svg'
-    filename = os.path.join(root, 'certs', fname)
-    with open(filename, 'w') as fh:
-        fh.write(template.render(configs = config))
+    # root = path to output directory
+    fname = (
+        config["name"]["first_name"]
+        + "_"
+        + config["name"]["last_name"]
+        + "_"
+        + config["date"]
+        + ".svg"
+    )
+    filename = os.path.join(root, "certs", fname)
+    with open(filename, "w") as fh:
+        fh.write(template.render(configs=config))
+
+
 def load_template():
     root = os.path.dirname(os.path.abspath(__file__))
-    templates_dir = os.path.join(root, 'templates')
-    env = Environment( loader = FileSystemLoader(templates_dir) )
-    template = env.get_template('cert_template.svg')
+    templates_dir = os.path.join(root, "templates")
+    env = Environment(loader=FileSystemLoader(templates_dir))
+    template = env.get_template("cert_template.svg")
     return template
+
 
 def write_templates(config):
     # filename = parse_args()
     template = load_template()
     write_svg_cert(template, config)
 
+
 def main(config):
     write_templates(config)
 
 
-if __name__ == '__main__':
-
-    config = {'name': {'first_name': 'matt',
-                       'last_name' : 'biddle'},
-              'date': '2023-05-19',
-              }
+if __name__ == "__main__":
+    config = {
+        "name": {"first_name": "matt", "last_name": "biddle"},
+        "date": "2023-05-19",
+    }
 
     main(config)
